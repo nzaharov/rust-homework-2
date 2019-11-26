a045accc718bd73efdb4687fb89a570f2ab95270
Nikolay Zaharov <nzaharov988@gmail.com>
2019-11-27 00:14:06 +0200

    better main method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use solution::Packet;
 
 fn main() {
-    let packet = Packet::from_source(b"rbcd", 3);
+    let (packet, remainder) = Packet::from_source(b"rbcd", 3);
     println!("{:?}", packet);
+    println!("{:?}", packet.payload());
 }
